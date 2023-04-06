a24cb22b0129319f85771b61f65733900eda32fb
alphadevking <danielose266@gmail.com>
2023-04-06 23:27:10 +0100

    Update forecast.tsx

--- src/pages/forecast.tsx
+++ src/pages/forecast.tsx
@@ -3,6 +3,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { WiDaySunny, WiCloudy, WiRain, WiSnow, WiRainWind } from 'react-icons/wi';
 import { Loading } from '@nextui-org/react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 interface ForecastData {
     location: {
@@ -93,62 +94,70 @@ export default function Forecast() {
 }
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 mx-auto">
-            <h1 className="text-3xl font-bold mb-8">Weather Forecast</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="flex flex-col mb-4">
-                    <label htmlFor="city" className="text-lg font-semibold mb-2">
-                        City
-                    </label>
-                    <input
-                        type="text"
-                        id="city"
-                        name="city"
-                        className="py-2 px-4 rounded-lg shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="py-2 px-4 rounded-lg shadow-md bg-[#A3D4F7] hover:bg-[#89c1e8] text-slate-600 w-fit transition-colors"
-                >
-                    Get Forecast
-                </button>
-            </form>
-            {isLoading && <p className="mt-4"><Loading /></p>}
-            {error && (
-                <p className="mt-4 text-red-500">
-                    An error occurred: {error.message}
-                </p>
-            )}
-            {forecast && (
-                <div className="mt-8">
-                    <h2 className="text-xl font-semibold mb-2 text-center">
-                        Forecast for {forecast.location.name}, {forecast.location.country}
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 md:gap-5">
-                        {forecast.forecast.forecastday.map((day) => (
-                            <div
-                                key={day.date}
-                                className="my-2 p-2 border-2 border-blue-500 rounded-lg flex items-center"
-                            >
-                                <span className="mr-2 text-3xl">{getWeatherIcon(day.day.condition.text)}</span>
-                                <div>
-                                    <p className="text-lg font-semibold">{getFormattedDate(day.date)}</p>
-                                    <p className="text-md font-medium">{day.day.condition.text}</p>
-                                    <p className="text-md font-medium">
-                                        High: {day.day.maxtemp_c}°C / Low: {day.day.mintemp_c}°C
-                                    </p>
-                                </div>
+        <>
+            <Head>
+                <title>CleonCast | Forecast</title>
+            </Head>
+
+            <main>
+                <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 mx-auto">
+                    <h1 className="text-3xl font-bold mb-8">Weather Forecast</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="flex flex-col mb-4">
+                            <label htmlFor="city" className="text-lg font-semibold mb-2">
+                                City
+                            </label>
+                            <input
+                                type="text"
+                                id="city"
+                                name="city"
+                                className="py-2 px-4 rounded-lg shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="py-2 px-4 rounded-lg shadow-md bg-[#A3D4F7] hover:bg-[#89c1e8] text-slate-600 w-fit transition-colors"
+                        >
+                            Get Forecast
+                        </button>
+                    </form>
+                    {isLoading && <p className="mt-4"><Loading /></p>}
+                    {error && (
+                        <p className="mt-4 text-red-500">
+                            An error occurred: {error.message}
+                        </p>
+                    )}
+                    {forecast && (
+                        <div className="mt-8">
+                            <h2 className="text-xl font-semibold mb-2 text-center">
+                                Forecast for {forecast.location.name}, {forecast.location.country}
+                            </h2>
+                            <div className="grid grid-cols-1 md:grid-cols-3 md:gap-5">
+                                {forecast.forecast.forecastday.map((day) => (
+                                    <div
+                                        key={day.date}
+                                        className="my-2 p-2 border-2 border-blue-500 rounded-lg flex items-center"
+                                    >
+                                        <span className="mr-2 text-3xl">{getWeatherIcon(day.day.condition.text)}</span>
+                                        <div>
+                                            <p className="text-lg font-semibold">{getFormattedDate(day.date)}</p>
+                                            <p className="text-md font-medium">{day.day.condition.text}</p>
+                                            <p className="text-md font-medium">
+                                                High: {day.day.maxtemp_c}°C / Low: {day.day.mintemp_c}°C
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    )}
+                    <Link className='opacity-70 py-3 text-sm hover:underline text-center' href={'/'} passHref>
+                        Check out the current weather condition at any location!
+                    </Link>
                 </div>
-            )}
-            <Link className='opacity-70 py-3 text-sm hover:underline text-center' href={'/'} passHref>
-                Check out the current weather condition at any location!
-            </Link>
-        </div>
+            </main>
+        </>
     );
 }
